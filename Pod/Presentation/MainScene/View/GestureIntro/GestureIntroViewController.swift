eae723a56d869a2a533d976059948a4aa2fdaeb3
Kwontaewan <xc200925@gmail.com>
2020-06-25 16:15:28 +0900

    💄 add gesture intro
    add gesture intro vc

---
+++ Pod/Presentation/MainScene/View/GestureIntro/GestureIntroViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GestureIntroViewController.swift
+//  Pod
+//
+//  Created by Gunter on 2020/06/25.
+//  Copyright © 2020 Gunter. All rights reserved.
+//
+
+import UIKit
+
+class GestureIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
